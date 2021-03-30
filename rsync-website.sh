4d33bbac8894bebcdd75a7f167613e135da39784
Vic Mortelmans <vicmortelmans@gmail.com>
2021-03-30 14:02:39 +0200

    general overhaul

--- rsync-website.sh
+++ rsync-website.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 cd `dirname "$0"`
-curl -o Sarasota.html http://livemass.s3.amazonaws.com/sarasota/sarasota-mass-of-the-day.html
-curl -o Warrington.html http://livemass.s3.amazonaws.com/warrington/warrington-mass-of-the-day.html
-curl -o LA.html http://livemass.s3.amazonaws.com/la/la-mass-of-the-day.html
-curl -o Guadalajara.html http://livemass.s3.amazonaws.com/guadalajara/guadalajara-mass-of-the-day/index.html
-curl -o Fribourg.html http://livemass.s3.amazonaws.com/fribourg/fribourg-mass-of-the-day.html
+curl -o sarasota-d.html http://livemass.s3.amazonaws.com/sarasota/sarasota-mass-of-the-day.html
+curl -o warrington-d.html http://livemass.s3.amazonaws.com/warrington/warrington-mass-of-the-day.html
+curl -o la-d.html http://livemass.s3.amazonaws.com/la/la-mass-of-the-day.html
+curl -o guadalajara-d.html http://livemass.s3.amazonaws.com/guadalajara/guadalajara-mass-of-the-day/index.html
+curl -o fribourg-d.html http://livemass.s3.amazonaws.com/fribourg/fribourg-mass-of-the-day.html
 git pull
 git commit -am "update of livemass pages"
 git push origin master
