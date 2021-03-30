066cffeeba05a480497c25cce98137375415603e
Vic Mortelmans <vicmortelmans@gmail.com>
2021-03-30 15:40:42 +0200

    debugging

--- rsync-website.sh
+++ rsync-website.sh
@@ -2,8 +2,8 @@
 cd `dirname "$0"`
 curl -o sarasota-d.html http://www.livemass.net/locations/sarasota-d.html
 curl -o warrington-d.html http://www.livemass.net/locations/warrington-d.html
-curl -o la-d.html http://livemass.s3.amazonaws.com/la/la-mass-of-the-day.html
-curl -o guadalajara-d.html http://www.livemass.net/locations/la-d.html
+curl -o la-d.html http://www.livemass.net/locations/la-d.html
+curl -o guadalajara-d.html http://www.livemass.net/locations/guadalajara-d.html
 curl -o fribourg-d.html http://www.livemass.net/locations/fribourg-d.html
 git pull
 git commit -am "update of livemass pages"
