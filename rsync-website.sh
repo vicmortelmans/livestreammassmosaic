1a17ba76d041cc48f89da8629490a2ff259909f6
Vic Mortelmans <vicmortelmans@gmail.com>
2021-03-30 15:32:08 +0200

    update of livemass pages

--- rsync-website.sh
+++ rsync-website.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 cd `dirname "$0"`
-curl -o sarasota-d.html http://livemass.s3.amazonaws.com/sarasota/sarasota-mass-of-the-day.html
-curl -o warrington-d.html http://livemass.s3.amazonaws.com/warrington/warrington-mass-of-the-day.html
+curl -o sarasota-d.html http://www.livemass.net/locations/sarasota-d.html
+curl -o warrington-d.html http://www.livemass.net/locations/warrington-d.html
 curl -o la-d.html http://livemass.s3.amazonaws.com/la/la-mass-of-the-day.html
-curl -o guadalajara-d.html http://livemass.s3.amazonaws.com/guadalajara/guadalajara-mass-of-the-day/index.html
-curl -o fribourg-d.html http://livemass.s3.amazonaws.com/fribourg/fribourg-mass-of-the-day.html
+curl -o guadalajara-d.html http://www.livemass.net/locations/la-d.html
+curl -o fribourg-d.html http://www.livemass.net/locations/fribourg-d.html
 git pull
 git commit -am "update of livemass pages"
 git push origin master
